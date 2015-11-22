9c0c30f083683c736815327175c63036bffbd5f1
Masaki Hasegawa <masaki.hasegawa@gmail.com>
2015-11-22 16:47:27 +0900

    update機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
@@ -11,27 +12,33 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to "posts/#{@post.id}"
-  end
+ def create
+   @post = Post.new(post_params)
+   @post.save
+   redirect_to "/posts/#{@post.id}"
+ end
 
-  def edit
+ def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+   redirect_to "/posts/#{@post.id}"
   end
 
-  def delete
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/"
   end
 
-  private
-  def post_params
-    params.require(:post).permit(
-      :title,
-      :category,
-      :body)
-  end
-
+ private
+   def post_params
+     params.require(:post).permit(
+       :title,
+       :category,
+       :body)
+   end
 end
